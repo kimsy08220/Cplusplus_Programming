a792bf15eae809924f48ea609f450135858959e0
kimsy08220 <kimsy08220@naver.com>
2022-04-02 00:08:16 +0900

    Chapter02

---
+++ Chapter02/9.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main(void)
+{
+	string name, address, age;
+
+	cout << "이름은? ";
+	getline(cin, name);
+	cout << "주소는? ";
+	getline(cin, address);
+	cout << "나이는? ";
+	getline(cin, age);
+
+	cout << name << ", " << address << ", " << age << "세"<< endl;
+}
