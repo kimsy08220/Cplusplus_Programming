3c62d573653642b715e9c7ad1f5d3ef6b8a75325
kimsy08220 <kimsy08220@naver.com>
2022-04-15 00:06:43 +0900

    Chapter03

---
+++ Chapter03/6.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+using namespace std;
+
+class EvenRandom {
+public:
+	EvenRandom();
+	int next();
+	int nextInRange(int start, int end);
+};
+
+EvenRandom::EvenRandom() {
+	srand((unsigned)time(0));
+}
+
+int EvenRandom::next() {
+	int n = rand();
+	if (n % 2 == 0)
+		return n;
+	else
+		return --n;
+}
+
+int EvenRandom::nextInRange(int start, int end) {
+	int n = rand() % (end - start + 1) + start;
+	if (n % 2 == 0)
+		return n;
+	else
+		return --n;
+}
+
+int main(void)
+{
+	EvenRandom r;
+	cout << "-- 0에서 " << RAND_MAX << "까지의 랜덤 정수 10개 --" << endl;
+	for (int i = 0; i < 10; i++) {
+		int n = r.next();
+		cout << n << ' ';
+	}
+	cout << endl << endl << "-- 2에서 " << "10까지의 랜덤 정수 10개 --" << endl;
+	for (int i = 0; i < 10; i++) {
+		int n = r.nextInRange(2, 10);
+		cout << n << ' ';
+	}
+	cout << endl;
+}
