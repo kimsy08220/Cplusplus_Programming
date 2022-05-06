d9cd143aeb295c205ff43e9f35ade26da3cd0252
kimsy08220 <kimsy08220@naver.com>
2022-05-07 00:18:21 +0900

    Chapter06

---
+++ Chapter06/1.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+using namespace std;
+
+// (1)
+//int add(int a[], int size)
+//{
+//	int sum = 0;
+//	for (int i = 0; i < size; i++)
+//		sum += a[i];
+//	return sum;
+//}
+//
+//int add(int a[], int size, int b[])
+//{
+//	int sum = 0;
+//	for (int i = 0; i < size; i++)
+//		sum += a[i] + b[i];
+//	return sum;
+//}
+
+// (2)
+int add(int a[], int size, int b[]=NULL)
+{
+	int sum = 0;
+	for (int i = 0; i < size; i++) {
+		sum += a[i];
+		if (b != NULL)
+			sum += b[i];
+	}
+	return sum;
+}
+
+int main(void)
+{
+	int a[] = {1,2,3,4,5};
+	int b[] = {6,7,8,9,10};
+	int c = add(a,5);
+	int d = add(a,5,b);
+	cout << c << endl;
+	cout << d << endl;
+}
