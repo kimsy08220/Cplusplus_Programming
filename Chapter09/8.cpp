6d3136af6842b2ff816dce770367c6733302b23a
KSY <50660653+kimsy08220@users.noreply.github.com>
2022-05-27 18:17:56 +0900

    Chapter09

--- Chapter09/8.cpp
+++ Chapter09/8.cpp
@@ -39,7 +39,7 @@ int main(void) {
 	p[0] = new Oval("빈대떡", 10, 20);
 	p[1] = new Rect("찰떡", 30, 40);
 	p[2] = new Triangular("토스트", 30, 40);
-	
+
 	for (int i = 0; i < 3; i++)
 		cout << p[i]->getName() << "넓이는 " << p[i]->getArea() << endl;
 	for (int i = 0; i < 3; i++)
