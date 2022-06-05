23a71a1a1a41b3b396bb307201bd2173f5763040
kimsy08220 <kimsy08220@naver.com>
2022-06-06 00:20:33 +0900

    Chapter11

---
+++ Chapter11/5.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main(void) {
+	string cmd;
+
+	while (true) {
+		cout << "종료하려면 exit을 입력하세요 >> ";
+		getline(cin, cmd);
+		if (cmd == "exit") {
+			cout << "프로그램을 종료합니다...." << endl;
+			return 0;
+		}
+	}
+}
